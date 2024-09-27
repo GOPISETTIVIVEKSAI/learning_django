c6928ba402ac500630aa974c3b948b367fd0a442
gopisettiviveksai <viveksaigopisetti@gmail.com>
2024-09-27 14:14:08 +0530

    started django

--- myproject/eco/views.py
+++ myproject/eco/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    #return HttpResponse(
+     #    """<h1>Welcome to Website Bro inka em chestham codding tappa ekkuvu aiythey dobbidhi code jagraktha </h1>
+      #   <h5>Bavvundanna</h5>
+       # <p>Mathu vadalara</p>""")
+    return render(request, 'home.html')
